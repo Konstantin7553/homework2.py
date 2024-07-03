12d83df724340eb2fe1fd203b6a5e8c5b8f4e074
Konstantin7553 <krasik150@gmail.com>
2024-07-03 14:40:02 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+Vypolnennye_DZ = 'всего задач: 12'
+Zatrachennye_chasy = 'затраченных часов: 1.5'
+Nazvanie = 'Курс:Python'
+a = 12
+b = 1.5
+Vrevya_na_odno_zadanie = 'среднее время выполнения: 0.125'
+print(b / a)
+print(Nazvanie, Vypolnennye_DZ, Zatrachennye_chasy, Vrevya_na_odno_zadanie, sep=",")
